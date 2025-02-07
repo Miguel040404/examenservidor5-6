9120971c190ef41021596fccd61c231b1970a66f
Miguel040404 <mmaldonadocobo4@gmail.com>
2025-02-07 18:24:03 +0100

    Ejercicio 2

---
+++ src/lib/data.js
@@ -0,0 +1,56 @@
+'use server'
+
+import prisma from "@/lib/prisma"
+
+// -------------------------------- Repartidor
+
+async function obtenerRepartidores() {
+    const repartidores = await prisma.repartidor.findMany()
+    return repartidores
+}
+
+async function obtenerRepartidor(id) {
+    const repartidor = await prisma.repartidor.findUnique({
+        where: { id: +id }
+    })
+    return repartidor
+}
+
+// -------------------------------- Pedido
+
+async function obtenerPedidos() {
+    const pedidos = await prisma.pedido.findMany()
+    return pedidos
+}
+
+async function obtenerPedido(id) {
+    const pedido = await prisma.pedido.findUnique({
+        where: { id: +id }
+    })
+    return pedido
+}
+
+// -------------------------------- Pizza
+
+async function obtenerPizzas() {
+    const pizzas = await prisma.pizza.findMany()
+    return pizzas
+}
+
+async function obtenerPizza(id) {
+    const pizza = await prisma.pizza.findUnique({
+        where: { id: +id }
+    })
+    return pizza
+}
+
+// ------------------------------------------------
+
+export {
+    obtenerRepartidores,
+    obtenerRepartidor,
+    obtenerPedidos,
+    obtenerPedido,
+    obtenerPizzas,
+    obtenerPizza
+}
